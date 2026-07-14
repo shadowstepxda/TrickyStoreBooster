662dfe85a983817ef40efa547f185823f921938c
Nipun Narang <nipunnarang12@gmail.com>
2026-07-14 11:44:55 +0530

    Update service.sh
    Replace full package dump with DenyList/Exclude List targeting

--- service.sh
+++ service.sh
@@ -1,19 +1,19 @@
 MODDIR=${0%/*}
+. "$MODDIR/common.sh"
 
+# Wait for the device to be unlocked
 while [ "$(/system/bin/app_process -Djava.class.path=$MODDIR/isKeyguardLocked.dex /system/bin com.rosan.shell.ActiviteJava)" == "true" ];
 do
 	sleep 2
 done
 
 sleep 10
-su -c "sh -c \"pm list packages | sed 's/^package://' > /data/adb/tricky_store/target.txt\""
-sleep 1
 
-function auto_game_mode_node () {
+# Regenerate target.txt immediately post boot, then every 12 hours
+function sync_target_list_loop () {
 	while true ; do
-	su -c "sh -c \"pm list packages | sed 's/^package://' > /data/adb/tricky_store/target.txt\""
-
-	sleep 43200
+		sync_target_list
+		sleep 43200
 	done
 }
-auto_game_mode_node &
+sync_target_list_loop &
